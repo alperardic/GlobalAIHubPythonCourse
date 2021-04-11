4561b88ea905080d9ecda4a128bbfade3521d0b9
Alper Ardıç <77046207+alperardic@users.noreply.github.com>
2021-04-11 16:06:58 +0300

    finalmodule.py is updated

---
+++ Final Project/finalmodule.py
@@ -0,0 +1,13 @@
+def soru():
+    sorular = {"1" : ["Karadeniz bölgesinde 18 il bulunmaktadır?\nTrue or False ?","True"],
+               "2" : ["En küçük asal sayı kaçtır?\nA)1 B)-1 C)0 D)2 ?","D"],
+               "3" : ["Sağlıklı bir insanın vücudunda kaç kemik bulunur?\nA)310 B)250 C)206 D)419","C"],
+               "4" : ["Alternatif akımın mucidi kimdir?\nA)Thomas Edison B)Nicola Tesla C)Elon Musk D)Einstein","B"],
+               "5" : ["Albert Einstein nerede doğmuştur?\nA)Almanya, B)Fransa, C)İngiltere, D)Amerika","A"],
+               "6" : ["Aşağıdakilerden hangisi Dünya'ya en yakındır?\nA)Venüs B)Merkür C)Mars D)Jüpiter","B"],
+               "7" : ["Avusturalya'nın başkenti aşağıdakilerden hangisidir?\nA)Ottawa B)Budapeşte C)Sidney D)Kanberra","D"],
+               "8" : ["Magna Carta hangi ülkenin kralıyla yapılmış bir sözleşmedir?\nA)Fransa B)İngiltere C)İspanya D)Almanya","B"],
+               "9" : ["ABD başkanlarından John Fitzgerald Kennedy’e suikast düzenleyerek öldüren kimdir?\nA)Jack Ruby B)Lee Harvey Oswald C)Clay Shaw D)Nobody","B"],
+               "10": ["Fransız İhtilali kaç yılında gerçekleşmiştir?\nA)1789-1799 B)1768-1787 C)1876-1889 D)1890-1901","A"]
+               }
+    return sorular
